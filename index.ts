c3dfde68edc9918d8dc31b59f012514b979dff37
Roberto Torres <5945088-RobertoTorresKavak@users.noreply.gitlab.com>
2021-09-07 20:20:10 -0500

    ✨ -se agrega el orm para el levantamiento de la base de datos

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
+'use strict'
+const mongoose = require('mongoose');
+require('custom-env').env(true)
 import ip from "ip";
 import app from "./app";
 
-app.listen(3000, () => {
-  console.log(`API REST corriendo en http://${ip.address()}:${3000}/`)
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}
+
+const port = 3000;
+
+mongoose.connect(process.env.DB_HOST, connectionOptions,(err, res) => {
+  if(err) {
+      return console.error(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log(`Conexion a la base de datos en: ${process.env.DB_HOST}` )
+
+  app.listen(port, () => {
+      console.log(`API REST corriendo en http://${ip.address()}:${port}/`)
+  });
 });
