4ca5a693c7a8f40016e53763225f13f87e1b4ab7
Roberto Torres <5945088-RobertoTorresKavak@users.noreply.gitlab.com>
2021-09-08 13:08:06 -0500

    🔥 se elimina middleware de errors

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-import { errorHandler } from './src/middlewares/error-handler';
 import { corsHandler } from './src/middlewares/cors-handler'
 import { typeCase } from './src/middlewares/type-case'
 import Express, { Application } from 'express';
@@ -11,7 +10,5 @@ app.use(Express.json({ limit: '50mb' }));
 app.use(corsHandler());
 app.use(typeCase('snake'));
 app.use("/user", userRouter)
-
-app.use(errorHandler);
 
 export default app;
